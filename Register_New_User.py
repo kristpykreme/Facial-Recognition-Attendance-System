72f9995af569fc30fdf53846bd68ddcb77cb2f49
kristpykreme <64589735+kristpykreme@users.noreply.github.com>
2021-03-18 05:20:51 +0800

    Add files via upload

---
+++ Register_New_User.py
@@ -0,0 +1,88 @@
+''''
+Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
+
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18    
+
+'''
+
+import cv2
+import os
+import numpy as np
+from PIL import Image
+
+def Register_New_User(face_id):
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640) # set video width
+    cam.set(4, 480) # set video height
+
+    face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # Initialize individual sampling face count
+    count = 0
+
+    while(True):
+
+        ret, img = cam.read()
+        img = cv2.flip(img, 1)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+        for (x,y,w,h) in faces:
+
+            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
+            count += 1
+
+            # Save the captured image into the datasets folder
+            cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+
+            cv2.imshow('image', img)
+
+        k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+        if k == 27:
+            break
+        elif count >= 100: # Take 100 face sample and stop video
+             break
+
+    # Do a bit of cleanup
+    print("\n User in system. Exiting Program :)")
+    cam.release()
+    cv2.destroyAllWindows()
+
+    # Path for face image database
+    path = 'dataset'
+
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+
+    # function to get the images and label data
+    def getImagesAndLabels(path):
+
+        imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
+        faceSamples=[]
+        ids = []
+
+        for imagePath in imagePaths:
+
+            PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+            img_numpy = np.array(PIL_img,'uint8')
+
+            id = int(os.path.split(imagePath)[-1].split(".")[1])
+            faces = detector.detectMultiScale(img_numpy)
+
+            for (x,y,w,h) in faces:
+                faceSamples.append(img_numpy[y:y+h,x:x+w])
+                ids.append(id)
+
+        return faceSamples,ids
+
+    print ("\n Training faces. This take a few seconds. Wait ...")
+    faces,ids = getImagesAndLabels(path)
+    recognizer.train(faces, np.array(ids))
+
+    # Save the model into trainer/trainer.yml
+    recognizer.write('trainer/trainer.yml')
+
+    # Print the numer of faces trained and end program
+    print("\n {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+
+
